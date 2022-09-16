3e151de50f6557c5ed41f54b6f82a1569b1b1075
chrisochen <chrischentan@hotmail.com>
2022-09-15 20:12:13 -0600

    Attempt to work with github pages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "/portfoliov2/" : "",
+  basePath: "/portfoliov2",
   images: {
     unoptimized: true,
   },
